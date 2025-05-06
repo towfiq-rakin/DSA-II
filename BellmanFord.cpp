258f27787c2d4b61a07b0516a46cbf94ec350b66
Towfiq Omar Rakin <towfiqomarrakin@gmail.com>
2025-05-07 02:40:01 +0600

    BellmanFord

--- BellmanFord.cpp
+++ BellmanFord.cpp
@@ -1,45 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = INT_MAX;
-
 struct Edge {
     int u, v, w;
 };
 
-int n, m, v;
 vector<Edge> edges;
 vector<int> d;
 
 void solve() {
-    d.assign(n, INF);
+    int n = edges.size();
     d[0] = 0;
     for (int i = 0; i < n - 1; ++i)
         for (Edge e : edges)
             if (d[e.u] < INF)
                 d[e.v] = min(d[e.v], d[e.u] + e.w);
-    // display d, for example, on the screen
 }
 
-void printPath(int v) {
-    if (d[v] == INF) {
-        cout << "No path to " << v << endl;
-    } else {
-        cout << "Shortest path to " << v << " is " << d[v] << endl;
+void print() {
+    for(int i = 0; i < d.size(); ++i) {
+        if(d[i] == INF) cout << "INF" << endl;
+        else cout <<"0 to "<< i << " : "<< d[i] << endl;
     }
 }
 
 int main() {
+    int n, m;
     cin >> n >> m ;
     edges.resize(m);
+    d.resize(n, INF);
     for (int i = 0; i < m; ++i) {
         int u, v, w;
         cin >> u >> v >> w;
         edges[i] = {u, v, w};
     }
     solve();
-
-    for (int i = 0; i < n; ++i) {
-        printPath(i);
-    }
-    return 0;
+    print();
 }
+
+/*
+input:
+6 9
+0 1 6
+0 2 4 
+0 3 5
+1 4 -1
+2 1 -2
+2 4 3
+3 2 -2
+3 5 -1
+4 5 3
+*/
